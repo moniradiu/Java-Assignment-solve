780d11b7d90624d22e85307e86162befd0011c3a
moniradiu <moniradie@gmail.com>
2018-01-02 11:38:45 +0600

    Write the Java code of a program that reads one number from the user, and prints it back to show which number was entered by the user.

---
+++ Problem3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Problem3 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Please enter your number:");
+		int n=sc.nextInt();
+		System.out.println("You Entered:"+n);
+	}
+
+}
