72c74d616455f32d2c95ab3504c36d79d808f4fb
moniradiu <moniradie@gmail.com>
2018-01-02 11:50:06 +0600

    Write the Java code of a program that reads two numbers from the user and prints “first” if the first number is greater than the second number.

---
+++ Problem6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Problem6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter First Number: ");
+		int n1=sc.nextInt();
+		
+		System.out.println("Enter Second Number: ");
+		int n2=sc.nextInt();
+		
+		if(n1 > n2){
+			System.out.println("first");
+		}
+
+	}
+
+}
