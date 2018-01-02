5dfb1a2977524da3501afee3500faab6b6f41a58
moniradiu <moniradie@gmail.com>
2018-01-02 11:35:32 +0600

    Write the Java code of a program that prints the first ten even positive whole numbers. (Do NOT use loop or modulus operator for this problem).

---
+++ Problem2.java
@@ -0,0 +1,15 @@
+
+public class Problem2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	 System.out.println(2);
+	 System.out.println(4);
+	 System.out.println(6);
+	 System.out.println(8);
+	 System.out.println(10);
+
+	}
+
+}
